4a21a4794017f466bd482f4a321b45d4897a4af6
Elli0ttH0rne <elliott.horne@griffithuni.edu.au>
2024-09-01 02:08:03 +1000

    Added additional users + formatting

--- 3813ICT-Assessment/src/app/auth.service.ts
+++ 3813ICT-Assessment/src/app/auth.service.ts
@@ -5,12 +5,35 @@ import { Injectable } from '@angular/core';
 })
 export class AuthService {
   private defaultUsers = [
-    { username: 'user1', password: 'user1', email: 'user1@gmail.com', securityLevel: 'user' },
-    { username: 'user2', password: 'user2', email: 'user2@gmail.com', securityLevel: 'user' },
-    { username: 'user3', password: 'user3', email: 'user3@gmail.com', securityLevel: 'user' },
-    { username: 'groupAdmin1', password: 'groupAdmin1', email: 'groupAdmin1@gmail.com', securityLevel: 'groupAdmin' },
-    { username: 'groupAdmin2', password: 'groupAdmin2', email: 'groupAdmin2@gmail.com', securityLevel: 'groupAdmin' },
-    { username: 'super', password: '123', email: 'super1@gmail.com', securityLevel: 'superAdmin' },
+    { 
+      username: 'user1', 
+      password: 'user1', 
+      email: 'user1@gmail.com', 
+      securityLevel: 'user' 
+    },
+    { 
+      username: 'user2', 
+      password: 'user2', 
+      email: 'user2@gmail.com', 
+      securityLevel: 'user' 
+    },
+    { 
+      username: 'user3', 
+      password: 'user3', 
+      email: 'user3@gmail.com', 
+      securityLevel: 'user' 
+    },
+    { username: 'groupAdmin1', 
+      password: 'groupAdmin1', 
+      email: 'groupAdmin1@gmail.com', 
+      securityLevel: 'groupAdmin' 
+    },
+    { 
+      username: 'super', 
+      password: '123', 
+      email: 'super1@gmail.com', 
+      securityLevel: 'superAdmin' 
+    },
   ];
 
   constructor() {
